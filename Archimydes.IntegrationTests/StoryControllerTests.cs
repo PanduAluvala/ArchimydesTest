43ce70a8e2608d9da03af147c6a9fb96cfd05b4d
pandu <pandureddy0254@gmail.com>
2020-11-09 11:33:06 -0500

    update refactor remove not used controllers

--- Archimydes.IntegrationTests/StoryControllerTests.cs
+++ Archimydes.IntegrationTests/StoryControllerTests.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Archimydes.Controllers;
+using Archimydes.DataAccessLayer;
+using Moq;
 using NUnit.Framework;
 
 namespace Archimydes.IntegrationTests
 {
-    [TestFixture]
-    class StoryControllerTests
-    {
-    }
 }
